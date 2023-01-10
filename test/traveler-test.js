c3089764b2154d0a8bdd02c2a86bf396018a64c2
Rae Gebhart <raegebhart@gmail.com>
2023-01-10 16:48:17 -0700

    test: Add should be function test

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,4 +1,16 @@
 import { expect } from 'chai';
-import travellerData from  '../src/data/travelers'
-import Traveller from '../src/Traveler'
+import travelerData from  '../src/data/travelers'
+import Traveler from '../src/Traveler'
 
+describe('Traveler', () => {
+  let traveler
+  
+  beforeEach(() => {
+    traveler = new Traveler(travelerData)
+  })
+
+  it('Should be a function', () => {
+    expect(Traveler).to.be.a('function')
+  })
+
+})
