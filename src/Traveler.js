ef371ecf65d7609a243a61891fd98a48741d3026
Rae Gebhart <raegebhart@gmail.com>
2023-01-10 18:25:47 -0700

    test: Passed sad path test for getFirstName

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,13 +1,13 @@
 class Traveler {
-  constructor(traveler) {
-    this.id = traveler.id
-    this.name = traveler.name
-    this.travelerType = traveler.travelerType
-    this.amountSpent = 0
+  constructor(traveler = 0) {
+    this.id = traveler.id;
+    this.name = traveler.name;
+    this.travelerType = traveler.travelerType;
+    this.amountSpent = 0;
   }
   getFirstName() {
-    return this.name.split(' ')[0]
+    return !this.id ? "Can't find that user" : this.name.split(" ")[0];
   }
 }
 
-export default Traveler
+export default Traveler;
