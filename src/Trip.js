593c97a570171d8843eed708b3f88a4f444a96c7
Rae Gebhart <raegebhart@gmail.com>
2023-01-11 12:00:18 -0700

    test: Pass destinations propery test

--- src/Trip.js
+++ src/Trip.js
@@ -1,16 +1,17 @@
-class Trip {
-  constructor(tripData) {
-    this.data = tripData
+class Trips {
+  constructor(tripsData, destinationData) {
+    this.tripsData = tripsData
+    this.destinationData = destinationData
   }
   getTripsById(userID) {
-    const trips = this.data.filter(trip => trip.userID === userID)
+    const trips = this.tripsData.filter(trip => trip.userID === userID)
     return trips.length === 0 ? 'Can\'t find that user' : trips
   }
 
   getTripsByStatus(status) {
-    const tripsByStatus = this.data.filter(trip => trip.status === status)
+    const tripsByStatus = this.tripsData.filter(trip => trip.status === status)
     return tripsByStatus.length === 0 ? 'No trips found with that status' : tripsByStatus
   }
 }
 
-export default Trip
+export default Trips
