cfe2a3a5b11d04e13b60082d44a29ed1f0ef9919
Rae Gebhart <raegebhart@gmail.com>
2023-01-11 08:19:14 -0700

    test: Passed test for getTripsById method

--- src/Trip.js
+++ src/Trip.js
@@ -2,6 +2,9 @@ class Trip {
   constructor(tripData) {
     this.data = tripData
   }
+  getTripsById(userID) {
+    return this.data.filter(trip => trip.userID === userID)
+  }
 }
 
 export default Trip
