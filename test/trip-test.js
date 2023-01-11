62890c0847e63702e4ec891353fc341124010459
Rae Gebhart <raegebhart@gmail.com>
2023-01-11 09:28:21 -0700

    test: Add sad path test for getTripsByStatus

--- test/trip-test.js
+++ test/trip-test.js
@@ -31,4 +31,39 @@ describe('Trip', () => {
   it('Should return Can\'t find that user if id doesn\'t exist', () => {
     expect(trip.getTripsById(51)).to.equal('Can\'t find that user')
   })
+
+  it('Should find all trips with provided status', () => {
+    expect(trip.getTripsByStatus('pending')).to.deep.equal([ {
+      id: 3,
+      userID: 3,
+      destinationID: 22,
+      travelers: 4,
+      date: "2022/05/22",
+      duration: 17,
+      status: "pending",
+      suggestedActivities: [],
+    }, {
+      id: 6,
+      userID: 29,
+      destinationID: 35,
+      travelers: 3,
+      date: "2022/06/29",
+      duration: 9,
+      status: "pending",
+      suggestedActivities: [],
+    }, {
+      id: 9,
+      userID: 24,
+      destinationID: 19,
+      travelers: 5,
+      date: "2022/12/19",
+      duration: 19,
+      status: "pending",
+      suggestedActivities: [],
+    }])
+  })
+
+  it('Should return No trips found with that status if no trips found with status', () => {
+    expect(trip.getTripsByStatus()).to.equal('No trips found with that status')
+  })
 })
