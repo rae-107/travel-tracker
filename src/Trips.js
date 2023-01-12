4a337f940cbcbd6d02b36c136430f39b1c167d5d
Rae Gebhart <raegebhart@gmail.com>
2023-01-12 13:44:07 -0700

    bug: Fix calculateTripsByYear method to include agent fee

--- src/Trips.js
+++ src/Trips.js
@@ -1,34 +1,44 @@
 class Trips {
   constructor(tripsData, destinationData) {
-    this.tripsData = tripsData
-    this.destinationData = destinationData
+    this.tripsData = tripsData;
+    this.destinationData = destinationData;
   }
   getTripsById(userID) {
-    const trips = this.tripsData.filter(trip => trip.userID === userID)
-    return trips.length === 0 ? 'Can\'t find that user' : trips
+    const trips = this.tripsData.filter((trip) => trip.userID === userID);
+    return trips.length === 0 ? "Can't find that user" : trips;
   }
 
   getDestinationByDestinationId(destinationId) {
-    const destination = this.destinationData.find(destination => destination.id === destinationId)
-    return !destination ? 'Can\'t find a destination by that id' : destination
+    const destination = this.destinationData.find(
+      (destination) => destination.id === destinationId
+    );
+    return !destination ? "Can't find a destination by that id" : destination;
   }
 
   getTripsByStatus(status) {
-    const tripsByStatus = this.tripsData.filter(trip => trip.status === status)
-    return tripsByStatus.length === 0 ? 'No trips found with that status' : tripsByStatus
+    const tripsByStatus = this.tripsData.filter(
+      (trip) => trip.status === status
+    );
+    return tripsByStatus.length === 0
+      ? "No trips found with that status"
+      : tripsByStatus;
   }
 
   calculateTripsThisYear(userID, date) {
     const tripsByYear = this.getTripsById(userID)
-    .filter(trip => trip.date.slice(0, 4) === date.slice(0, 4))
-    .reduce((total, trip) => {
-      const lodgingCost = this.getDestinationByDestinationId(trip.destinationID).estimatedLodgingCostPerDay * trip.duration
-      const flightCost = this.getDestinationByDestinationId(trip.destinationID).estimatedFlightCostPerPerson * trip.travelers
-      total += lodgingCost + flightCost
-      return total
-    }, 0)
-    return tripsByYear
+      .filter((trip) => trip.date.slice(0, 4) === date.slice(0, 4))
+      .reduce((total, trip) => {
+        const lodgingCost =
+          this.getDestinationByDestinationId(trip.destinationID)
+            .estimatedLodgingCostPerDay * trip.duration;
+        const flightCost =
+          this.getDestinationByDestinationId(trip.destinationID)
+            .estimatedFlightCostPerPerson * trip.travelers;
+        total += lodgingCost + flightCost;
+        return total;
+      }, 0);
+    return tripsByYear + tripsByYear * 0.1;
   }
 }
 
-export default Trips
+export default Trips;
