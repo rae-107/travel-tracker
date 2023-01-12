7fbe1f098d50e36754d5d934fc95d0930818bc85
Rae Gebhart <raegebhart@gmail.com>
2023-01-12 12:01:50 -0700

    test: Add should be a function test

---
+++ test/Trip-test.js
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import Trip from '../src/Trip'  
+import tripData from '../src/data/trips'
+
+describe('Trip', () => {
+  let trip
+
+  beforeEach(() => {
+    trip = new Trip(tripData.slice(0, 1))
+  })
+
+  it("Should be a function", () => {
+    expect(Trip).to.be.a("function");
+  });
+})
